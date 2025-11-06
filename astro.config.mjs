3b30d78c32130ba19d85b132bd3caf60f9dfb14f
Kevin Nunez <thefilthysoap@gmail.com>
2025-11-05 20:16:39 -0500

    Initial Astro project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://KevinNunez-dev.github.io',
+  base: '/rpt-clinic-landing',
+  output: 'static',
+  build: {
+    assets: '_astro'
+  }
+});
